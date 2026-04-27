20907d15fafc7a1423a48974cfed15a8746ed1e0
Kauan Oliveira Freitas <kauanffreitas57@gmail.com>
2026-04-27 19:58:44 -0300

    Agora o backend notifica o discord quando for desligado; criando script para executar em produção

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -44,3 +44,25 @@ app.listen(port, () => {
 
     sendMessage(`🛰️ **Sistema Iniciado!**\n\n**Acesse o Painel:**\n${config.network.frontendUrl}\n\n**Servidores Disponíveis:**\n${serverList}`);
 });
+
+const gracefulShutdown = async () => {
+    console.log('\nFinalizando processos...');
+
+    try {
+        await sendMessage("⚠️ O sistema foi desligado.");
+    } catch (err: any) {
+        console.error("Erro ao notificar:", err.message);
+    } finally {
+        process.exit(0);
+    }
+    
+};
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (text: string) => {
+    if (text.trim().toLowerCase() === 'exit' || text.trim().toLowerCase() === 'stop') {
+        gracefulShutdown();
+    }
+});
