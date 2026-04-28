8b534a19212bb0fd9ead262abaa0e524f7fad259
Kauan Oliveira Freitas <kauanffreitas57@gmail.com>
2026-04-28 00:57:22 -0300

    Criando queryService; renomeando discordServices para discordService

--- backend/src/services/serverServices.ts
+++ backend/src/services/serverServices.ts
@@ -1,82 +1,13 @@
 import { spawn } from 'child_process';
-import { GameDig } from 'gamedig';
 import config from '../../config.json' with { type: 'json' };
 import AppError from '../utils/AppError.js';
-import type { Server, ServerStatus } from '../types/types.js'
+import type { Server } from '../types/types.js'
 import { Rcon } from 'rcon-client';
 import 'dotenv/config';
-import { sendMessage } from './discordServices.js';
+import { sendMessage } from './discordService.js';
+import { serverCache } from './queryService.js';
 
 const rconPassword = process.env.RCON_PASSWORD || "";
-
-const serverCache = new Map<number, Server>();
-
-setInterval(async () => {
-    for (const server of config.servers) {
-        const cached = serverCache.get(server.id);
-
-        if (cached?.stoppingTimestamp && Math.abs(cached.stoppingTimestamp - Date.now()) < 60000) {
-            continue;
-        }
-
-        try {
-            const state = await GameDig.query({
-                type: 'minecraft',
-                host: '127.0.0.1',
-                port: server.port,
-                socketTimeout: 200,
-                attemptTimeout: 500,
-                maxRetries: 1
-            });
-
-            let currentInactiveTimestamp: number | null = cached?.inactiveTimestamp || null;
-
-            if (state.players.length < 1) {
-
-                if (!currentInactiveTimestamp) {
-                    currentInactiveTimestamp = Date.now();
-                    const timestamp = new Date().toLocaleTimeString('pt-BR');
-                    console.log(`[${timestamp}] [SISTEMA]: Servidor inativo fechará em 30 minuto`);
-                } else {
-                    const inactiveDuration = Date.now() - currentInactiveTimestamp;
-
-                    if (inactiveDuration >= 1800000) {
-                        const timeStr = new Date().toLocaleTimeString('pt-BR');
-                        console.log(`[${timeStr}] [SISTEMA]: ${server.name} inativo há 30min. Desligando...`);
-                        stopServer(server.id, "SISTEMA");
-                        return;
-                    }
-                }
-            } else {
-                currentInactiveTimestamp = null;
-            }
-
-            serverCache.set(server.id, {
-                id: server.id, 
-                name: server.name,
-                port: server.port,
-                path: server.path,
-                status: "ONLINE", 
-                players: state.players,
-                stoppingTimestamp: null,
-                inactiveTimestamp: currentInactiveTimestamp
-            });
-        } catch (e) {
-            let newStatus: ServerStatus = cached?.status == "STARTING" ? "STARTING" : "OFFLINE";
-
-            serverCache.set(server.id, {
-                id: server.id, 
-                name: server.name,
-                port: server.port,
-                path: server.path,
-                status: newStatus,
-                players: [],
-                stoppingTimestamp: null,
-                inactiveTimestamp: null
-            });
-        }
-    }
-}, 5000);
 
 const startServer = async (serverId: number) => {
     const server: Server | undefined = serverCache.get(serverId);
