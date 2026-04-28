e78523df8c3d16aa4d909da8693a9a1db02ef0ca
Kauan Oliveira Freitas <kauanffreitas57@gmail.com>
2026-04-28 02:40:01 -0300

    refatorando o queryService

--- backend/src/services/queryService.ts
+++ backend/src/services/queryService.ts
@@ -8,8 +8,24 @@ const serverCache = new Map<number, Server>();
 setInterval(async () => {
     for (const server of config.servers) {
         const cached = serverCache.get(server.id);
+        let newServerStatus: Server = {
+                id: server.id, 
+                name: server.name,
+                port: server.port,
+                path: server.path,
+                status: "OFFLINE", 
+                players: [],
+                stoppingTimestamp: null,
+                inactiveTimestamp: null
+        };
 
-        if (cached?.stoppingTimestamp && Math.abs(cached.stoppingTimestamp - Date.now()) < 60000) {
+        if (cached) {
+            newServerStatus = { ...cached };
+        }
+
+        // Mantenho o status STOPPING por 1 minuto para dar tempo de criar backup e fechar corretamente antes de alguém tentar abrir o servidor de novo
+        if (newServerStatus.stoppingTimestamp && Math.abs(newServerStatus.stoppingTimestamp - Date.now()) < 60000) {
+            newServerStatus.inactiveTimestamp = null;
             continue;
         }
 
@@ -25,49 +41,41 @@ setInterval(async () => {
 
             let currentInactiveTimestamp: number | null = cached?.inactiveTimestamp || null;
 
+            //Se não tiver players no servidor
             if (state.players.length < 1) {
-
+                //E não existia ainda o tempo de inatividade ele cria o timestamp
                 if (!currentInactiveTimestamp) {
                     currentInactiveTimestamp = Date.now();
                     const timestamp = new Date().toLocaleTimeString('pt-BR');
-                    console.log(`[${timestamp}] [SISTEMA]: Servidor inativo fechará em 30 minuto`);
-                } else {
+                    console.log(`[${timestamp}] [SISTEMA]: ${newServerStatus.name} inativo fechará em 30 minuto`);
+                } else { //Se já tinha um timestamp ele verifica se já passou 30 minutos de inatividade
                     const inactiveDuration = Date.now() - currentInactiveTimestamp;
 
                     if (inactiveDuration >= 1800000) {
                         const timeStr = new Date().toLocaleTimeString('pt-BR');
-                        console.log(`[${timeStr}] [SISTEMA]: ${server.name} inativo há 30min. Desligando...`);
-                        stopServer(server.id, "SISTEMA");
+                        console.log(`[${timeStr}] [SISTEMA]: ${newServerStatus.name} inativo há 30min. Desligando...`);
+                        stopServer(newServerStatus.id, "SISTEMA"); //Se tiver passado ele desliga o servidor
                         continue;
                     }
                 }
-            } else {
+            } else { //Se tiver players ele deixa null o timestamp
                 currentInactiveTimestamp = null;
             }
 
-            serverCache.set(server.id, {
-                id: server.id, 
-                name: server.name,
-                port: server.port,
-                path: server.path,
-                status: "ONLINE", 
-                players: state.players,
-                stoppingTimestamp: null,
-                inactiveTimestamp: currentInactiveTimestamp
-            });
+            newServerStatus.status = "ONLINE";
+            newServerStatus.players = state.players;
+            newServerStatus.stoppingTimestamp = null;
+            newServerStatus.inactiveTimestamp = currentInactiveTimestamp;
+
+            serverCache.set(server.id, newServerStatus);
         } catch (e) {
             let newStatus: ServerStatus = cached?.status == "STARTING" ? "STARTING" : "OFFLINE";
 
-            serverCache.set(server.id, {
-                id: server.id, 
-                name: server.name,
-                port: server.port,
-                path: server.path,
-                status: newStatus,
-                players: [],
-                stoppingTimestamp: null,
-                inactiveTimestamp: null
-            });
+            newServerStatus.status = newStatus;
+            newServerStatus.stoppingTimestamp = null;
+            newServerStatus.inactiveTimestamp = null;
+
+            serverCache.set(server.id, newServerStatus);
         }
     }
 }, 5000);
